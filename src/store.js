da126d2c733c1048b97a66387e20e88c8cf77a66
Tzuyang Tsai <14104502+shiyou0130011@users.noreply.github.com>
2025-01-23 13:23:41 +0800

    修正 store.get 的使用

--- src/store.js
+++ src/store.js
@@ -1,14 +1,14 @@
 import { load } from '@tauri-apps/plugin-store'
 
 const store = await load('wdc-store.json', { autoSave: false })
-store.get<{ value: number }>('rem-val')
+store.get('rem-val')
 	.then(function(val){
 		if(val && val > 0){
 			document.querySelector("#rem").value = val 
 			document.querySelector("#rem").dispatchEvent(new CustomEvent("change"))
 		}
 	})
-store.get<{ value: number }>('vw-val')
+store.get('vw-val')
 	.then(function(val){
 		if(val && val > 0){
 			document.querySelector("#vw").value = val 
