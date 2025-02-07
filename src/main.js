72512f12f61eabc315079ba4e589b1517ab405ae
Tzuyang Tsai <14104502+shiyou0130011@users.noreply.github.com>
2025-02-07 09:43:55 +0800

    Hotfix: 修正各 input 之間的計算

--- src/main.js
+++ src/main.js
@@ -11,25 +11,25 @@ window.addEventListener("DOMContentLoaded", function () {
 		document.querySelector("#calc-rem").value = Math.round(Number(document.querySelector("#calc-px").value) / this.value * calcRound) / calcRound
 	})
 	document.querySelector("#vw").addEventListener("change", function () {
-		document.querySelector("#calc-vw").value = Math.round(Number(document.querySelector("#calc-px").value) / this.value * calcRound) / calcRound
+		document.querySelector("#calc-vw").value = Math.round(Number(document.querySelector("#calc-px").value) / this.value * calcRound) / calcRound * 100
 	})
-
+	
 	document.querySelector("#calc-rem").addEventListener("change", function () {
 		document.querySelector("#calc-px").value = Math.round(Number(document.querySelector("#rem").value) * this.value * calcRound) / calcRound
-
+	
 		document.querySelector("#calc-vw").value = Math.round(Number(document.querySelector("#calc-px").value) / Number(document.querySelector("#vw").value) * calcRound) / calcRound * 100
 	})
 	document.querySelector("#calc-px").addEventListener("change", function () {
 		document.querySelector("#calc-rem").value = Math.round(Number(this.value) / Number(document.querySelector("#rem").value) * calcRound) / calcRound
-
-		document.querySelector("#calc-vw").value = Math.round(Number(this.value) / Number(document.querySelector("#vw").value) * calcRound) / calcRound
+	
+		document.querySelector("#calc-vw").value = Math.round(Number(this.value) / Number(document.querySelector("#vw").value) * calcRound) / calcRound * 100
 	})
 	document.querySelector("#calc-vw").addEventListener("change", function () {
 		document.querySelector("#calc-px").value = Math.round(Number(document.querySelector("#vw").value) * this.value * calcRound) / calcRound / 100
-		document.querySelector("#calc-rem").value = Math.round(Number(document.querySelector("#calc-px").value) / Number(document.querySelector("#rem").value) * calcRound) / calcRound / 100
-
+		document.querySelector("#calc-rem").value = Math.round(Number(document.querySelector("#calc-px").value) / Number(document.querySelector("#rem").value) * calcRound) / calcRound 
+	
 	})
-
+	
 	Array.prototype.forEach.call(document.querySelectorAll("input[type=number]"), function (input) {
 		input.addEventListener("input", function () {
 			input.dispatchEvent(new Event("change"))
